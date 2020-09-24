39b5344162a75bc702571eedb0c4e41be70114ef
fabianschmauder <fabianschmauder@gmail.com>
2020-09-24 14:49:28 +0200

    Add Parameterized test

--- src/test/java/de/neuefische/maven/StoreCapacityTest.java
+++ src/test/java/de/neuefische/maven/StoreCapacityTest.java
@@ -1,100 +1,29 @@
 package de.neuefische.maven;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StoreCapacityTest {
 
-    @Test
-    public void storeIsFullRedTest () {
+  @ParameterizedTest(name = "AlarmLevel: {0} and customers {1} should be {2}")
+  @CsvSource({
+      "Rot, 1,Zu viele Personen",
+      "Rot, 0, Maximale Personenzahl nicht überschritten",
+      "Gelb, 31, Zu viele Personen",
+      "Gelb, 30, Maximale Personenzahl nicht überschritten",
+      "Grün, 61, Zu viele Personen",
+      "Grün, 60, Maximale Personenzahl nicht überschritten",
 
-        //GIVEN
-        int customersCount = 1;
-        String alarmLevel = "Rot";
+  })
+  public void checkAlarmLevel(String alarmLevel, int customersCount, String expected) {
+    //WHEN
+    String alarmString = StoreCapacity.storeFullAlarm(customersCount, alarmLevel);
 
-        //WHEN
-        String alarmString = StoreCapacity.storeFullAlarm(customersCount, alarmLevel);
+    //THEN
+    assertEquals(expected, alarmString);
 
-        //THEN
-        assertEquals("Zu viele Personen", alarmString);
-
-    }
-
-    @Test
-    public void storeIsNotFullRedTest () {
-
-        //GIVEN
-        int customersCount = 0;
-        String alarmLevel = "Rot";
-
-        //WHEN
-        String alarmString = StoreCapacity.storeFullAlarm(customersCount, alarmLevel);
-
-        //THEN
-        assertEquals("Maximale Personenzahl nicht überschritten", alarmString);
-
-    }
-
-    @Test
-    public void storeIsFullYellowTest () {
-
-        //GIVEN
-        int customersCount = 31;
-        String alarmLevel = "Gelb";
-
-        //WHEN
-        String alarmString = StoreCapacity.storeFullAlarm(customersCount, alarmLevel);
-
-        //THEN
-        assertEquals("Zu viele Personen", alarmString);
-
-    }
-
-    @Test
-    public void storeIsNotFullYellowTest () {
-
-        //GIVEN
-        int customersCount = 30;
-        String alarmLevel = "Gelb";
-
-        //WHEN
-        String alarmString = StoreCapacity.storeFullAlarm(customersCount, alarmLevel);
-
-        //THEN
-        assertEquals("Maximale Personenzahl nicht überschritten", alarmString);
-
-    }
-
-    @Test
-    public void storeIsFullGreenTest () {
-
-        //GIVEN
-        int customersCount = 61;
-        String alarmLevel = "Grün";
-
-        //WHEN
-        String alarmString = StoreCapacity.storeFullAlarm(customersCount, alarmLevel);
-
-        //THEN
-        assertEquals("Zu viele Personen", alarmString);
-
-    }
-
-    @Test
-    public void storeIsNotFullGreenTest () {
-
-        //GIVEN
-        int customersCount = 60;
-        String alarmLevel = "Grün";
-
-        //WHEN
-        String alarmString = StoreCapacity.storeFullAlarm(customersCount, alarmLevel);
-
-        //THEN
-        assertEquals("Maximale Personenzahl nicht überschritten", alarmString);
-
-    }
-
+  }
 
 }
